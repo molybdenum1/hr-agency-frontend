8e85e2e5a599b096244cd38fae96203c80c2f311
molybdenum1 <kozelden4ik@gmail.com>
2023-04-22 22:11:13 +0300

    start page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="start_page">
+        <div className="start">
+          <div className="header">
+            <div className="navbar">
+              <h2>Ninja Hunter</h2>
+              <div className="nav-menu">
+                <div className="nav-item">Jobs</div>
+                <div className="nav-item">Companies</div>
+                <div className="nav-item">Salaries</div>
+                <div className="nav-item">Careers</div>
+              </div>
+              <div className="nav-right">
+                <div className="log-btn">Login</div>
+                <div className="log-btn">Reg</div>
+                <div className="log-btn-search">Post Jobs</div>
+              </div>
+            </div>
+          </div>
+          <div className="start-center">
+            <div className="center-left">
+              <div className="center-h">Find your dream job</div>
+              <div className="search-center">Search</div>
+            </div>
+            <div className="center-right">
+              <div className="center-img">
+                <img
+                  src="https://brookwoods.com/wp-content/uploads/hiring-hr.png"
+                  alt="search"
+                  width={'720px'}
+                  height={'337px'}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
